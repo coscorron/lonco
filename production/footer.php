74a261d5461e7c55846749305fab989087f8dbde
Rodrigo Amigo <rodrigo.amigo@gmail.com>
2018-06-24 21:08:42 -0400

    diseño de lonco
    diseño final a aplicar a Lonco

---
+++ production/footer.php
@@ -0,0 +1,8 @@
+<!-- footer content -->
+<footer>
+  <div class="pull-right">
+    Lonco -  <a href="https://www.connectis-chile.com/">Connectis 2018</a>
+  </div>
+  <div class="clearfix"></div>
+</footer>
+<!-- /footer content -->
